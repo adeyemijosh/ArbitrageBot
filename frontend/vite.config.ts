87f648e01e1c4cebd64b56e8b33002b3f4c1df01
Josh adeyemi(0xBerry) <70116919+adeyemijosh@users.noreply.github.com>
2026-03-19 11:21:46 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,5 +12,8 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true
-  }
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
